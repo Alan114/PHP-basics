240c26e6c09d8c52f501c5ce46dc0f2ae1c6fa90
Alan Pecenkovic <alanquad@yahoo.com>
2023-03-13 15:02:07 +0100

    variables in PHP

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
    echo "<strong>Hello world!</strong>";
+
+   $name = "Alan";
+
+   define("MESSAGE", "Hello friends!");
+
+   echo "<p>My name is $name</p>";
+
+   echo "<p>I'd like to say " . MESSAGE . "</p>";
+
+   $number = 45;
+
+   $calculation = $number * 31 / 97 + 4;
+
+   echo "The result of the calculation is $calculation";
+
+   $myBool = true;
+
+   echo "<p>This statement is true? " . $myBool . "<p>";
+
+   echo $myBool ? "true" : "false";
+   echo "<br>";
+
+   $variableName = "name";
+
+   echo $$variableName;
 ?>
